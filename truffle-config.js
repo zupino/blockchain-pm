8287200bd7d75bf08611da4f289d633b8ba51d8a
Marco Z <eng.marco.zunino@gmail.com>
2020-04-14 21:53:06 +0200

    enable websocket to enable Drizzle working with local ganache
    We noticed in our first round of drizzle that the whole thing did not work if we used an instance of ganache as local chain, but the same Drizzle app worked fine if we were running `truffle develop` instead and using that as local chain. It seems like the issue is because the Drizzle provider needs to use websocket to receive chain events, which were not enabled in truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -51,6 +51,7 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 9545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
+      websockets: true        // Enable EventEmitter interface for web3 (default: false)
      },
 
     // Another network with more advanced options...
