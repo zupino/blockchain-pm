ad31f17b87925fa7462c2b0208cee52416409bd7
Marco Zunino <eng.marco.zunino@gmail.com>
2020-04-25 18:29:23 +0100

    Selection of ERC20 needed import from fresh OpenZeppelin git clone

---
+++ contracts/Drop.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.5.11;
+
+import "./token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {}
---
+++ contracts/TransferToken.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.11;
+
+import 'Token.sol';
+
+
+contract TransferToken {
+
+}
