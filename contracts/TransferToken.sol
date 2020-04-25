ce30683c4196597c5f107f1d7f02b4df38c63e54
Marco Zunino <eng.marco.zunino@gmail.com>
2020-04-25 21:55:30 +0100

    main function for delegated token transfer

--- contracts/TransferToken.sol
+++ contracts/TransferToken.sol
@@ -8,4 +8,20 @@ contract TransferToken {
 		Drop token = Drop(0x73EA86DdC523D299916aa1d089652706B4C3D460);
 		token.transfer(msg.sender, 100);
 	}
+
+	function transferFrom(address recipient, uint amount) external {
+		Drop token = Drop(0x73EA86DdC523D299916aa1d089652706B4C3D460);
+		token.transferFrom(msg.sender, recipient, amount);
+	}
+}
+
+contract Owner {
+
+	function transfer(address recipient, uint amount) external {
+		Drop token = Drop(0x73EA86DdC523D299916aa1d089652706B4C3D460);
+		token.approve(0xtransfertokenaddress, amount);
+		TransferToken transferToken = TransferToken(0xtransfertokenaddress);
+		transferToken.transferFrom(recipient, amount)
+	}
+
 }
