5173560052e87868c218f7757816c29fbdafeeed
Marco Zunino <eng.marco.zunino@gmail.com>
2020-04-27 21:42:22 +0100

    Main tasks properties

--- contracts/Task.sol
+++ contracts/Task.sol
@@ -2,13 +2,25 @@ pragma solidity ^0.6.2;
 
 contract Task {
 
-	uint 	id;
+	uint	id;
+	// TODO not necessary, just for now
+	string 	name;
+	// 0: new           3: closed
+	// 1: accepted      4: cancelled
+	// 2: resolved      5: onhold
+
+	uint	status; 
 	address	project;
 	address	assignee;
 	address	oracle;
-	
-	// TODO might not be necessary
-	string	name;
+
+	uint deadlineAssignment;    // unix epoch timestamp of deadline
+	uint deadlineDelivery;      // unix epoch timestamp of delivery
+	uint compensationOracle;
+	uint compensationAssignee;  // estimated compensation
+	uint depositOracle;         // Oracle lock a deposit until task is completed
+	uint depositAssignee;       // Assignee too
+
 
 	constructor(string memory _name, address _project, uint _id) public {
 		name = _name;
