341ca07fe3a174d8a5355c035c66f98d8a69367a
Marco Zunino <eng.marco.zunino@gmail.com>
2020-05-10 21:54:30 +0100

    Modified App.js to align to Project contract

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import drizzleOptions from "./drizzleOptions.js";
+import { LoadingContainer } from "drizzle-react-components";
+import { DrizzleProvider } from "drizzle-react";
 import './App.css';
+
+import MyComponent from "./MyComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DrizzleProvider options={drizzleOptions}>
+        <LoadingContainer>
+            <MyComponent />
+        </LoadingContainer>
+    </DrizzleProvider>
   );
 }
 
