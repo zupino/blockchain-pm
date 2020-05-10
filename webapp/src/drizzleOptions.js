792f01d0cf3a89ea9c618a4cbc8636e38eee2ae7
Marco Zunino <eng.marco.zunino@gmail.com>
2020-05-10 21:41:20 +0100

    added webapp/ without node_modules, install drizzle

---
+++ webapp/src/drizzleOptions.js
@@ -0,0 +1,13 @@
+import Storage from "./contracts/Project.json";
+
+const options = {
+	contracts: [Project],
+	web3: {
+		fallback: {
+			type: "ws",
+			url: "ws://127.0.0.1:9545",
+		},
+	}
+};
+
+export default options;
