1c32138d29ba21645cd9a877302a3fbeaab6bcff
Marco Zunino <eng.marco.zunino@gmail.com>
2020-05-10 22:00:08 +0100

    Added value to migration call, for project budget

--- migrations/2_token_migration.js
+++ migrations/2_token_migration.js
@@ -3,7 +3,9 @@ const Project = artifacts.require("Project");
 const Task = artifacts.require("Task");
 
 module.exports = function(deployer) {
-	deployer.deploy(Project);
+// We deploy Project with 250000 test-wei as this
+// will generate 250000 DRP as project budget
+	deployer.deploy(Project, {value: 250000});
 	// deployer.deploy(ProjectExternalTask);
 	// deployer.deploy(Task);
 
