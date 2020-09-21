b148d473dfd68847fcd7118e956c617bc9a5b315
Marco Zunino <eng.marco.zunino@gmail.com>
2020-09-21 22:00:28 +0200

    export function to get ProjectList contract address

--- custom-scaffold/public/js/constants.js
+++ custom-scaffold/public/js/constants.js
@@ -10,15 +10,21 @@
 	    return uri;
 
     } 
+
+	function getProjectListAddress() {
+		var address = '0xA498730423825C107497E491b6C8Bca67BF65e0B';
+
+		return address;
+	}
    
     // Export the function to exports 
     // In node.js this will be exports  
     // the module.exports 
     // In browser this will be function in 
     // the global object sharedModule 
-    exports.getNgrokURI = getNgrokURI; 
+    exports.getNgrokURI = getNgrokURI;
+	exports.getProjectListAddress = getProjectListAddress;
        
 })(typeof exports === 'undefined'?  
-	this['constants']={}: exports); 
-
+	this['constants']={}: exports);
 
