77f64d77a392216f0110773b649c9decaf38aa7e
Marco Zunino <eng.marco.zunino@gmail.com>
2020-10-20 22:14:20 +0200

    Capitalize user addresses to make comparison work

--- custom-scaffold/public/js/userModule.js
+++ custom-scaffold/public/js/userModule.js
@@ -23,13 +23,23 @@
 	
 	function getUserId(address) {
         var userIds = [];
+		// debug 
+		console.log("[getUserId()] Address being looked up: " + address.toUpperCase());
+
+		// 	Looks weird because also the `x` in `0x` is capitalize, but this simplify the implementation
+		//	and address the fact that in different places Ether address have different capitalization
 
         // AnaMaria
-        userIds['0x13936ebf7f8e6420c79764532434a3f7cbdc36eb'] = '557058:7eff0acd-2d33-46d5-9b87-cbead45f3309';
+        userIds['0X13936EBF7F8E6420C79764532434A3F7CBDC36EB'] = '557058:7eff0acd-2d33-46d5-9b87-cbead45f3309';
         // Marco
-        userIds['0x2b64e6ff555d41fde621b3062df2aa81a081b33d'] = '557058:d26095ad-03ff-4c8b-886a-0662adc8dbdc';
+        userIds['0X2B64E6FF555D41FDE621B3062DF2AA81A081B33D'] = '557058:d26095ad-03ff-4c8b-886a-0662adc8dbdc';
 
-        return userIds[address];
+        console.log("[getUserId()] returned userId: " + userIds[address.toUpperCase()]);
+		console.log("Entire hash table: ");
+		console.log(userIds);
+		console.log("Type of received parameter `address`: " + typeof(address) );
+
+        return userIds[address.toUpperCase()];
 
     }
 
